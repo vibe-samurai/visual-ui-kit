a0ae033fac1abde7320b0f163c6e0941c1e3a4c0
breznovic <slymarkel@gmail.com>
2025-01-24 11:22:25 +0300

    feat: add text-area

---
+++ src/components/text-area/text-area.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TextArea } from './text-area'
+
+const meta: Meta<typeof TextArea> = {
+  argTypes: {
+    error: {
+      description: 'String describing the error message to display below the textarea.',
+    },
+    label: {
+      description: 'Label text to display above the textarea.',
+    },
+    disabled: {
+      description: 'Disables the textarea and applies disabled styles.',
+      control: 'boolean',
+    },
+  },
+  component: TextArea,
+  tags: ['autodocs'],
+  title: 'UI/TextArea',
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
+
+export const WithLabel: Story = {
+  args: {
+    label: 'Text Area',
+  },
+}
+
+export const WithError: Story = {
+  args: {
+    label: 'Text Area',
+    error: 'This field is required',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    label: 'Text Area',
+    disabled: true,
+  },
+}
---
+++ src/components/text-area/text-area.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx'
+import s from './text-area.module.scss'
+
+export type TextAreaProps = {
+  error?: string
+  label?: string
+  disabled?: boolean
+}
+
+export const TextArea = ({ error, label, disabled }: TextAreaProps) => {
+  return (
+    <div className={s.container}>
+      {label && <label className={clsx(s.label, { [s.disabled]: disabled })}>{label}</label>}
+      <textarea className={clsx({ [s.error]: error }, s.textarea)} disabled={disabled}></textarea>
+      {error && <span className={s.errorSpan}>{error}</span>}
+    </div>
+  )
+}
