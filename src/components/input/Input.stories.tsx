0ad9ca005ef3e4d95f6bfd7642d6e3002777dc26
Julia <veluat@gmail.com>
2025-01-26 23:47:58 +0300

    feat: add input component

---
+++ src/components/input/Input.stories.tsx
@@ -0,0 +1,87 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Input } from './Input'
+
+const meta = {
+  component: Input,
+  title: 'Components/Input',
+} satisfies Meta<typeof Input>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    id: 'email',
+    label: 'Email',
+    placeholder: 'Epam@epam.com',
+    type: 'email',
+  },
+}
+
+export const Password: Story = {
+  args: {
+    id: 'password',
+    label: 'Password',
+    placeholder: 'Epam@epam.com',
+    type: 'password',
+  },
+}
+
+export const SearchMode: Story = {
+  args: {
+    id: 'search',
+    placeholder: 'Input search',
+    type: 'search',
+  },
+}
+
+export const Disabled: Story = {
+  render: () => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', width: '100%' }}>
+      <Input
+        disabled
+        id={'disabled-input'}
+        label={'Email'}
+        placeholder={'Epam@epam.com'}
+        type={'text'}
+      />
+      <Input
+        disabled
+        id={'disabled-password'}
+        label={'Password'}
+        placeholder={'Password'}
+        type={'password'}
+      />
+      <div style={{ paddingTop: '20px' }}>
+        <Input disabled id={'disabled-search'} placeholder={'Input search'} type={'search'} />
+      </div>
+    </div>
+  ),
+}
+
+export const ErrorMessage: Story = {
+  args: {
+    errorMessage: 'Error text',
+    id: 'error-input',
+    label: 'Email',
+    placeholder: 'Epam@epam.com',
+    type: 'text',
+  },
+  render: args => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', width: '100%' }}>
+      <Input {...args} />
+      <Input {...{ ...args, id: 'error-password', label: 'Password', type: 'password' }} />
+      <Input
+        {...{
+          ...args,
+          id: 'error-search',
+          label: 'Search',
+          placeholder: 'Input search',
+          type: 'search',
+        }}
+      />
+    </div>
+  ),
+}
