890957cd2720e5b730d8559f61ca2edacb190f12
Julia <veluat@gmail.com>
2025-01-26 23:53:10 +0300

    refactor: configure settings and formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
+import * as path from 'path'
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import { dependencies, devDependencies } from './package.json'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es'],
+      name: 'visual-ui-kit',
+    },
+    rollupOptions: {
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+      ],
+      output: {
+        dir: 'dist',
+        globals: {
+          react: 'React',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+    ],
+  },
 })
