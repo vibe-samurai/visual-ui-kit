bb1e9fe4ef3c4fe0cb7fbdbaa319b5c13c1dd263
Julia <veluat@gmail.com>
2025-02-01 02:30:09 +0300

    refactor: update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,160 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierConfig from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  // Базовая конфигурация ESLint
+  js.configs.recommended,
+
+  // Глобальные переменные
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  // Конфигурация TypeScript
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      '@typescript-eslint': ts,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      ...ts.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-undef': 'off', // Отключаем no-undef для TypeScript
     },
-  }
-)
+  },
+
+  // Конфигурация React
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.jsx'], // Добавлен шаблон для JSX
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react/button-has-type': 'error',
+      'react/display-name': 'off',
+      'react/jsx-boolean-value': ['error'],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        { children: 'ignore', propElementValues: 'always', props: 'always' },
+      ],
+      'react/jsx-fragments': ['error'],
+      'react/void-dom-elements-no-children': ['error'],
+    },
+  },
+
+  // Конфигурация для импортов
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/extensions': [
+        'error',
+        { css: 'always', json: 'always', scss: 'always', svg: 'always' },
+      ],
+      'import/no-duplicates': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['sibling', 'parent'],
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/prefer-default-export': 'off',
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          paths: ['src'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  },
+
+  // Кастомные правила
+  {
+    rules: {
+      'arrow-parens': 'off',
+      'consistent-return': 'off',
+      curly: ['error', 'all'],
+      'max-lines': ['error', 300],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'off',
+      'no-duplicate-imports': 'error',
+      'no-empty-pattern': 'off',
+      'no-nested-ternary': 'error',
+      'no-undef': 'warn',
+      'no-unused-vars': 'off',
+      'no-var': 'error',
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', next: 'return', prev: '*' },
+        { blankLine: 'always', next: '*', prev: ['const', 'let', 'var'] },
+        {
+          blankLine: 'always',
+          next: 'return',
+          prev: ['if', 'for', 'while', 'switch', 'try'],
+        },
+        {
+          blankLine: 'any',
+          next: ['const', 'let', 'var'],
+          prev: ['const', 'let', 'var'],
+        },
+      ],
+      'prefer-const': 'error',
+    },
+  },
+
+  // Интеграция с Prettier
+  prettierConfig,
+]
