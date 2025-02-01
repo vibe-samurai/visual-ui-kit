4d438d55da87c000d4c6a3389c11b62df396e0db
Julia <veluat@gmail.com>
2025-02-01 12:18:40 +0300

    fix: fix dialog stories

--- src/components/dialog/Dialog.stories.tsx
+++ src/components/dialog/Dialog.stories.tsx
@@ -13,34 +13,59 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+const DefaultDialogWithState = (args: DialogProps) => {
+  const [open, setOpen] = useState(false)
+
+  function handleModalClosed() {
+    setOpen(false)
+  }
+  function handleModalOpened() {
+    setOpen(true)
+  }
+
+  return (
+    <div>
+      <Button onClick={handleModalOpened}>Open modal</Button>
+      <Dialog {...args} onClose={handleModalClosed} open={open} />
+    </div>
+  )
+}
+
+const TwoButtonsDialogWithState = (args: DialogProps) => {
+  const [open, setOpen] = useState(false)
+
+  function handleModalClosed() {
+    setOpen(false)
+  }
+  function handleModalOpened() {
+    setOpen(true)
+  }
+
+  return (
+    <div>
+      <Button onClick={handleModalOpened}>Open modal</Button>
+      <Dialog
+        {...args}
+        onCancelButtonClick={handleModalClosed}
+        onClose={handleModalClosed}
+        onConfirmButtonClick={handleModalClosed}
+        open={open}
+      />
+    </div>
+  )
+}
+
 export const Default: Story = {
   args: {
     open: true,
-    confirmButtonText: 'fadsfa',
+    confirmButtonText: 'confirm',
     onConfirmButtonClick: () => {
-      alert('first')
+      alert('Testing')
     },
     title: 'Modal',
     children: <p>Lorem text</p>,
   },
-  render: (args: DialogProps) => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [open, setOpen] = useState(false)
-
-    function handleModalClosed() {
-      setOpen(false)
-    }
-    function handleModalOpened() {
-      setOpen(true)
-    }
-
-    return (
-      <div>
-        <Button onClick={handleModalOpened}>Open modal</Button>
-        <Dialog {...args} onClose={handleModalClosed} open={open} />
-      </div>
-    )
-  },
+  render: args => <DefaultDialogWithState {...args} />,
 }
 
 export const TwoButtons: Story = {
@@ -55,28 +80,5 @@ export const TwoButtons: Story = {
     children: <p style={{ marginBottom: '18px' }}>Lorem text</p>,
     invertButtons: false,
   },
-  render: (args: DialogProps) => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [open, setOpen] = useState(false)
-
-    function handleModalClosed() {
-      setOpen(false)
-    }
-    function handleModalOpened() {
-      setOpen(true)
-    }
-
-    return (
-      <div>
-        <Button onClick={handleModalOpened}>Open modal</Button>
-        <Dialog
-          {...args}
-          onCancelButtonClick={handleModalClosed}
-          onClose={handleModalClosed}
-          onConfirmButtonClick={handleModalClosed}
-          open={open}
-        />
-      </div>
-    )
-  },
+  render: args => <TwoButtonsDialogWithState {...args} />,
 }
